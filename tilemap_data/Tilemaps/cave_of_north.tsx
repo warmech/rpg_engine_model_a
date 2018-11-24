7b6f5596c8c08595403fc94df8d0eb3b76508369
warmech <will_tooker@mac.com>
2018-11-24 01:16:32 -0600

    Tileset/Tilemap Addition

---
+++ tilemap_data/Tilemaps/cave_of_north.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="cave_of_north" tilewidth="16" tileheight="16" tilecount="30" columns="10">
+ <image source="../../dat/maps/09/tileset.png" width="160" height="48"/>
+</tileset>
